6c6eb28ad071faf178ae34de397793864e2eda17
xuedong <xuedong@example.com>
2025-01-14 18:03:40 +0800

    Add Wagtail to our blog

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,7 +27,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('', include('blog.urls')),
+    #path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('cms/', include(wagtailadmin_urls)),
-    path('pages/', include(wagtail_urls)),
+    #path('pages/', include(wagtail_urls)),
+    path('', include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
